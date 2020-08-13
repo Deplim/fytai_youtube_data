75d5cdeaaf79ef8bfdc003ca13ed5a53c476fcf4
Eui Ryeong <37531737+Deplim@users.noreply.github.com>
2020-08-13 14:30:34 +0900

    예외처리

--- get_youtube_data.py
+++ get_youtube_data.py
@@ -18,7 +18,7 @@ def storage_video_info(storage):
     return True
 
 
-def get_playlist_form_txt(target):
+def get_playlist_from_txt(target):
     channel_list=[]
 
     file=open(target, encoding='UTF8')
@@ -42,7 +42,7 @@ def get_video_list(target, key):
     video_list=[]
 
     request_url = "https://www.googleapis.com/youtube/v3/playlistItems?key="+key+"&part=snippet&playlistId="+target+"&maxResults=50"
-    print(request_url)
+    #print(request_url)
     comments_data = requests.get(request_url)
     jsonized_data = comments_data.json()
     for i in jsonized_data["items"]:
@@ -67,51 +67,57 @@ def get_comment_list(target_list, key):
     for i, v in enumerate(target_list):
         #print("i : ", i)
         temp_list=[]
-
         request_url = "https://www.googleapis.com/youtube/v3/commentThreads?key="+key+"&textFormat=plainText&part=snippet,replies&order=relevance&videoId="+v+"&maxResults=100"
-        #print(request_url)
-        comments_data = requests.get(request_url)
-        jsonized_data = comments_data.json()
-        for j in jsonized_data["items"]:
-            temp={}
-            temp["content"]=j["snippet"]["topLevelComment"]["snippet"]["textOriginal"].replace("\n", "")
-            temp["date"]=datetime.strptime(j["snippet"]["topLevelComment"]["snippet"]["updatedAt"], '%Y-%m-%dT%H:%M:%SZ')
-            temp["like"]=j["snippet"]["topLevelComment"]["snippet"]["likeCount"]
-            temp["replies"]=[]
-            if "replies" in j:
-                for m in j["replies"]["comments"]:
-                    rp_temp={}
-                    rp_temp["content"]=m["snippet"]["textOriginal"]
-                    rp_temp["date"]=datetime.strptime(m["snippet"]["updatedAt"], '%Y-%m-%dT%H:%M:%SZ')
-                    rp_temp["like"]=m["snippet"]["likeCount"]
-                    temp["replies"].append(rp_temp)
-            temp_list.append(temp)
-
-        while "nextPageToken" in jsonized_data:
-            nextPageToken=jsonized_data["nextPageToken"]
-
-            request_url = "https://www.googleapis.com/youtube/v3/commentThreads?key="+key+"&textFormat=plainText&part=snippet,replies&order=relevance&videoId=" + v + "&maxResults=100&pageToken="+nextPageToken
+        try:
             #print(request_url)
             comments_data = requests.get(request_url)
             jsonized_data = comments_data.json()
             for j in jsonized_data["items"]:
-                temp = {}
-                temp["content"] = j["snippet"]["topLevelComment"]["snippet"]["textOriginal"].replace("\n", "")
-                temp["date"] = datetime.strptime(j["snippet"]["topLevelComment"]["snippet"]["updatedAt"], '%Y-%m-%dT%H:%M:%SZ')
-                temp["like"] = j["snippet"]["topLevelComment"]["snippet"]["likeCount"]
-                temp["replies"] = []
+                temp={}
+                temp["content"]=j["snippet"]["topLevelComment"]["snippet"]["textOriginal"].replace("\n", "")
+                temp["date"]=datetime.strptime(j["snippet"]["topLevelComment"]["snippet"]["updatedAt"], '%Y-%m-%dT%H:%M:%SZ')
+                temp["like"]=j["snippet"]["topLevelComment"]["snippet"]["likeCount"]
+                temp["replies"]=[]
                 if "replies" in j:
                     for m in j["replies"]["comments"]:
-                        rp_temp = {}
-                        rp_temp["content"] = m["snippet"]["textOriginal"]
-                        rp_temp["date"] = datetime.strptime(m["snippet"]["updatedAt"], '%Y-%m-%dT%H:%M:%SZ')
-                        rp_temp["like"] = m["snippet"]["likeCount"]
+                        rp_temp={}
+                        rp_temp["content"]=m["snippet"]["textOriginal"]
+                        rp_temp["date"]=datetime.strptime(m["snippet"]["updatedAt"], '%Y-%m-%dT%H:%M:%SZ')
+                        rp_temp["like"]=m["snippet"]["likeCount"]
                         temp["replies"].append(rp_temp)
                 temp_list.append(temp)
 
-        comment_list.append(temp_list)
-        #print("\n-----------------------------------\n\n\n")
-        total.update(1)
+            while "nextPageToken" in jsonized_data:
+                nextPageToken=jsonized_data["nextPageToken"]
+
+                request_url = "https://www.googleapis.com/youtube/v3/commentThreads?key="+key+"&textFormat=plainText&part=snippet,replies&order=relevance&videoId=" + v + "&maxResults=100&pageToken="+nextPageToken
+                #print(request_url)
+                comments_data = requests.get(request_url)
+                jsonized_data = comments_data.json()
+                for j in jsonized_data["items"]:
+                    temp = {}
+                    temp["content"] = j["snippet"]["topLevelComment"]["snippet"]["textOriginal"].replace("\n", "")
+                    temp["date"] = datetime.strptime(j["snippet"]["topLevelComment"]["snippet"]["updatedAt"], '%Y-%m-%dT%H:%M:%SZ')
+                    temp["like"] = j["snippet"]["topLevelComment"]["snippet"]["likeCount"]
+                    temp["replies"] = []
+                    if "replies" in j:
+                        for m in j["replies"]["comments"]:
+                            rp_temp = {}
+                            rp_temp["content"] = m["snippet"]["textOriginal"]
+                            rp_temp["date"] = datetime.strptime(m["snippet"]["updatedAt"], '%Y-%m-%dT%H:%M:%SZ')
+                            rp_temp["like"] = m["snippet"]["likeCount"]
+                            temp["replies"].append(rp_temp)
+                    temp_list.append(temp)
+
+            comment_list.append(temp_list)
+            #print("\n-----------------------------------\n\n\n")
+            total.update(1)
+        except Exception as e:
+            print("get_comment_error:",e)
+            print(request_url)
+
+            comment_list.append([])
+            total.update(1)
     return comment_list
 
 
@@ -121,46 +127,61 @@ def get_video_info_list(target_list, key):
     for i, v in enumerate(target_list):
         #print("i : ", i)
         temp = {}
-
         request_url = "https://www.googleapis.com/youtube/v3/videos?key="+key+"&part=snippet,statistics&id=" + v
-        #print(request_url)
-        comments_data = requests.get(request_url)
-        jsonized_data = comments_data.json()
-        snippet=jsonized_data["items"][0]["snippet"]
-        statistics=jsonized_data["items"][0]["statistics"]
-        temp["channel"]=None
-        temp["title"]=snippet["title"]
-        temp["content"]=snippet["description"]
-        temp["date"]=datetime.strptime(snippet["publishedAt"], '%Y-%m-%dT%H:%M:%SZ')
-        temp["view"]=int(statistics["viewCount"])
-        temp["like"]=int(statistics["likeCount"])
-        temp["dislike"]=int(statistics["dislikeCount"])
-        temp["comments"]=None
-        temp["hash"]=v
 
-        info_list.append(temp)
-        #print("\n-----------------------------------\n\n\n")
+        try:
+            #print(request_url)
+            comments_data = requests.get(request_url)
+            jsonized_data = comments_data.json()
+            snippet=jsonized_data["items"][0]["snippet"]
+            statistics=jsonized_data["items"][0]["statistics"]
+            temp["channel"]=None
+            temp["title"]=snippet["title"]
+            temp["content"]=snippet["description"]
+            temp["date"]=datetime.strptime(snippet["publishedAt"], '%Y-%m-%dT%H:%M:%SZ')
+            temp["view"]=int(statistics["viewCount"])
+            temp["like"]=int(statistics["likeCount"])
+            temp["dislike"]=int(statistics["dislikeCount"])
+            temp["comments"]=None
+            temp["hash"]=v
+
+            info_list.append(temp)
+            #print("\n-----------------------------------\n\n\n")
+        except Exception as e:
+            print("get_video_error : ",e)
+            print(request_url)
+
+            temp["channel"] = None
+            temp["title"]=0
+            temp["comments"]=None
+            info_list.append(temp)
     return info_list
 
 
 if __name__ == '__main__':
     #print(get_hashed("1분과학", datetime.strptime("2016. 3. 20.", '%Y. %m. %d.')))
 
-    playlist=get_playlist_form_txt("playlist3.txt")
+    print("get playlist from txt >>> ", end="")
+    playlist=get_playlist_from_txt("playlist3_copy.txt")
     print("playlist length : "+str(len(playlist)), "\n")
 
     error_list=[]
-    key="AIzaSyBs-ft82koBxP1FyPvVbjPW6Z74gICSHS0"
+    key="AIzaSyDkKE6gjkpi8DiMa5ELStcQhNE0nLWIGnQ"
+
+    print("start !!! \n===============================\n===============================\n\n")
 
     for i in playlist:
         channel_hash=get_hashed(i["name"], datetime.strptime(i["regist"], '%Y. %m. %d.'))
 
+        print("get video list >>> ")
         video_list=get_video_list(i["playlist_id"], key)
-        print("video list length : "+str(len(video_list)))
+        print("video list length : "+str(len(video_list)), "\n")
 
+        print("get info list >>> ")
         video_info_list=get_video_info_list(video_list, key)
-        print("get video info list : complet\n")
+        print("complet\n")
 
+        print("get comment list >>> ")
         comment_list = get_comment_list(video_list, key)
         print("comment count for video list : ")
         print([len(k) for k in comment_list], "\n")
